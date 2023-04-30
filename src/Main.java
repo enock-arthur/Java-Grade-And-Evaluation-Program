6389aa8ad09e8f17feed706f6b81c765188ed71c
Enock Arthur <125463763+enockarthur01@users.noreply.github.com>
2023-04-30 09:14:23 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        /*This program assumes that the student has 8 subjects,
+        That is why I have created an array of 7.
+         */
+
+        int marks[] = new int[7];
+        int i;
+        float total = 0, average;
+        Scanner scanner = new Scanner(System.in);
+
+        for(i =0; i<7; i++){
+            System.out.println("Enter the marks of Subject "+(i+1)+":");
+            marks[i] = scanner.nextInt();
+            total = total + marks[i];
+
+        }
+
+        scanner.close();
+        //Calculating the average
+        average = total /8;
+        System.out.println("The student Grade is: ");
+        if(average>= 80){
+            System.out.print("A");
+        }
+
+        else if(average>=60 && average<80){
+            System.out.println("B");
+
+        }
+
+        else if (average >=40 && average<60){
+            System.out.println("C");
+        }
+
+        else{
+            System.out.println("D");
+        }
+
+
+    }
+}
